82f91f2134b2b19b06c85383431f04ce66056f31
samoylovivan <52442440+samoylovivan@users.noreply.github.com>
2019-09-10 23:32:56 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,38 @@
+package ivan.samoylov;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+public class Main {
+    public static final String PATH_TO_ZIP_LINUX = "/usr/lib/jvm/jdk-12.0.1/lib/src.zip";
+    public static final String PATH_TO_ZIP_WINDOWS = "C:\\Program Files\\Java\\jdk1.8.0_191\\src.zip";
+
+    public static void main(String[] args) {
+        readZipFile(PATH_TO_ZIP_LINUX);
+    }
+
+    private static void readZipFile(String zipFilePath) {
+        try {
+            ZipFile zipFile = new ZipFile(zipFilePath);
+
+            Enumeration<? extends ZipEntry> entries = zipFile.entries();
+
+            while (entries.hasMoreElements()) {
+                ZipEntry entry = entries.nextElement();
+                String name = entry.getName();
+                long compressedSize = entry.getCompressedSize();
+                long normalSize = entry.getSize();
+                String type = entry.isDirectory() ? "DIR" : "FILE";
+
+                System.out.println(name);
+                System.out.format("\t %s - %d - %d\n", type, compressedSize, normalSize);
+            }
+
+            zipFile.close();
+        } catch (IOException ex) {
+
+        }
+    }
+}
